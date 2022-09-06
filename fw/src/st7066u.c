c073d9a1740c887f19480b0a3e173d2155e04319
kiselev <kiselev2512@gmail.com>
2022-09-06 21:22:29 +0200

    Tabs removed

--- fw/src/st7066u.c
+++ fw/src/st7066u.c
@@ -10,7 +10,7 @@ static int col_count;
 
 static int ST7066U_Address(int row, int column) {
     if ((row < row_count) && (column < col_count)) {
-    	return 0x80 + ((row / 2) * 0x14) + ((row % 2) * 0x40) + column;
+        return 0x80 + ((row / 2) * 0x14) + ((row % 2) * 0x40) + column;
     } else {
         return 0x80;
     }
@@ -22,35 +22,35 @@ static void ST7066U_Locate(int row, int column) {
 }
 
 static void ST7066U_WriteTime(void) {
-	OUTPUTS_Set(OUTPUTS_LCD_EN, true);
+    OUTPUTS_Set(OUTPUTS_LCD_EN, true);
     TIMER_WaitMs(1);
-	OUTPUTS_Set(OUTPUTS_LCD_EN, false);
+    OUTPUTS_Set(OUTPUTS_LCD_EN, false);
     TIMER_WaitMs(1);
 }
 
 static void ST7066U_StartUp(void) {
     for(int i = 0; i < 3; i++) {
-    	OUTPUTS_Set(OUTPUTS_LCD_DB7, false);
-    	OUTPUTS_Set(OUTPUTS_LCD_DB6, false);
-    	OUTPUTS_Set(OUTPUTS_LCD_DB5, true);
-    	OUTPUTS_Set(OUTPUTS_LCD_DB4, true);
+        OUTPUTS_Set(OUTPUTS_LCD_DB7, false);
+        OUTPUTS_Set(OUTPUTS_LCD_DB6, false);
+        OUTPUTS_Set(OUTPUTS_LCD_DB5, true);
+        OUTPUTS_Set(OUTPUTS_LCD_DB4, true);
         ST7066U_WriteTime();
         TIMER_WaitMs(30);
     }
-	OUTPUTS_Set(OUTPUTS_LCD_DB7, false);
-	OUTPUTS_Set(OUTPUTS_LCD_DB6, false);
-	OUTPUTS_Set(OUTPUTS_LCD_DB5, true);
-	OUTPUTS_Set(OUTPUTS_LCD_DB4, false);
+    OUTPUTS_Set(OUTPUTS_LCD_DB7, false);
+    OUTPUTS_Set(OUTPUTS_LCD_DB6, false);
+    OUTPUTS_Set(OUTPUTS_LCD_DB5, true);
+    OUTPUTS_Set(OUTPUTS_LCD_DB4, false);
     ST7066U_WriteTime();
     TIMER_WaitMs(10);
 }
 
 static void ST7066U_WriteData(char value) {
     //Send upper four bits
-	OUTPUTS_Set(OUTPUTS_LCD_DB7, (value >> 7) & 0x01);
-	OUTPUTS_Set(OUTPUTS_LCD_DB6, (value >> 6) & 0x01);
-	OUTPUTS_Set(OUTPUTS_LCD_DB5, (value >> 5) & 0x01);
-	OUTPUTS_Set(OUTPUTS_LCD_DB4, (value >> 4) & 0x01);
+    OUTPUTS_Set(OUTPUTS_LCD_DB7, (value >> 7) & 0x01);
+    OUTPUTS_Set(OUTPUTS_LCD_DB6, (value >> 6) & 0x01);
+    OUTPUTS_Set(OUTPUTS_LCD_DB5, (value >> 5) & 0x01);
+    OUTPUTS_Set(OUTPUTS_LCD_DB4, (value >> 4) & 0x01);
     ST7066U_WriteTime();
     //Send lower four bits
     OUTPUTS_Set(OUTPUTS_LCD_DB7, (value >> 3) & 0x01);
@@ -61,12 +61,12 @@ static void ST7066U_WriteData(char value) {
 }
 
 static void ST7066U_WriteCommand(char value){
-	OUTPUTS_Set(OUTPUTS_LCD_RS, false);
+    OUTPUTS_Set(OUTPUTS_LCD_RS, false);
     ST7066U_WriteData(value);
 }
 
 static void ST7066U_WriteChar(char value){
-	OUTPUTS_Set(OUTPUTS_LCD_RS, true);
+    OUTPUTS_Set(OUTPUTS_LCD_RS, true);
     ST7066U_WriteData(value);
 }
 
@@ -90,7 +90,7 @@ static void ST7066U_Character(int row, int column,  char c) {
 }
 
 void ST7066U_Clear(void) {
-	ST7066U_WriteCommand(ST7066U_CMD_CLEARDISPLAY);
+    ST7066U_WriteCommand(ST7066U_CMD_CLEARDISPLAY);
     _row = 0;
     _column = 0;
 }
@@ -104,7 +104,7 @@ void ST7066U_Write(const char* text) {
         characterAmount++;
     }
     for (int i = 0; (i < characterAmount) && (i < row_count * col_count); i++) {
-    	ST7066U_Character(_row, _column, text[i]);
+        ST7066U_Character(_row, _column, text[i]);
     }
 }
 
@@ -117,17 +117,17 @@ void ST7066U_WriteLine(const char* text, int line) {
         characterAmount++;
     }
     for (int i = 0; (i < characterAmount)  && (i < col_count); i++) {
-    	ST7066U_Character(_row, _column, text[i]);
+        ST7066U_Character(_row, _column, text[i]);
     }
 }
 
 void ST7066U_WriteCharacter(const char c, int row, int column) {
-	ST7066U_Locate(row, column);
+    ST7066U_Locate(row, column);
     ST7066U_Character(_row, _column, c);
 }
 
 void ST7066U_WriteCharacters(const char* text, int row, int column) {
-	ST7066U_Locate(row, column);
+    ST7066U_Locate(row, column);
 
     int characterAmount =0;
 
@@ -135,16 +135,16 @@ void ST7066U_WriteCharacters(const char* text, int row, int column) {
         characterAmount++;
     }
     for (int i = 0; (i < characterAmount)  && (i < col_count - column); i++) {
-    	ST7066U_Character(_row, _column, text[i]);
+        ST7066U_Character(_row, _column, text[i]);
     }
 }
 
 void ST7066U_Init(int rows, int columns) {
-	row_count = rows;
-	col_count = columns;
+    row_count = rows;
+    col_count = columns;
 
-	// delay to wait for stable startup
-	TIMER_WaitMs(500);
+    // delay to wait for stable startup
+    TIMER_WaitMs(500);
     ST7066U_StartUp();
 
     ST7066U_WriteCommand(ST7066U_CMD_FUNCTIONSET);
